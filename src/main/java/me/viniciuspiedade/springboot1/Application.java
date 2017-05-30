c766a10218ba121d3b5735e768ab81f1d39a0477
vsouza <vinicius.rj@gmail.com>
2017-05-30 20:46:21 -0300

    cors update

--- src/main/java/me/viniciuspiedade/springboot1/Application.java
+++ src/main/java/me/viniciuspiedade/springboot1/Application.java
@@ -33,7 +33,8 @@ public class Application {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/*").allowedOrigins("http://localhost:3000");
+            	//registry.addMapping("/*").allowedOrigins("http://localhost:3000");
+            	registry.addMapping("/*").allowedOrigins("https://vpsouza-springboot1-react.firebaseapp.com");
             }
         };
     }
